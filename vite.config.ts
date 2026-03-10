018e5679c60f0eb5fcdf7650a236a8bc471367b2
Cao Hoai Chinh  Truong <chinhtch.quangngai@vss.local>
2026-03-10 15:57:46 +0700

    fix vite root path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './src',
+  root: "./frontend",
   build: {
-    outDir: '../dist',
-    minify: false,
+    outDir: "../dist",
     emptyOutDir: true,
-  },
+    minify: false
+  }
 });
